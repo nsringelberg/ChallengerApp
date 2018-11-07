aecf766c93f86fd4c2dd650373ac1f9c5584f51e
Nathan Ringelberg <nsringelberg@gmail.com>
2018-11-07 15:21:17 -0800

    challenges automatically generate from a list now --- maybe test with chrome?

---
+++ js/challengeData.js
@@ -0,0 +1,10 @@
+var games = {'Mario' : [{'ChallengeTitle' : 'beat the game naked',
+                         'ChallengeDescription' : 'be naked while beating the game',
+                         'difficulty' : 'border-warning'},
+                        {'ChallengeTitle' : 'beat the game without breathing',
+                         'ChallengeDescription' : 'dont breathe',
+                         'difficulty' : 'border-danger'}],
+             'Fortnite' : [{'ChallengeTitle' : 'win without building',
+                            'ChallengeDescription' : 'dont build',
+                            'difficulty' : 'border-danger'}]
+            }
