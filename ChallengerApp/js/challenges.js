7af6ef5f816a6ffc94d74f3f9d59c80a652b7629
nathanwerrede <nathanwerrede@gmail.com>
2018-11-08 21:28:58 -0800

    added my files(html for final)

--- ChallengerApp/js/challenges.js
+++ ChallengerApp/js/challenges.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+
   var source = $("#entry-template").html();
   var template = Handlebars.compile(source);
 
   var parentDiv = $("#templatedChallenges");
-
   for(var game in games){
     var challengeList = games[game];
     for (var i=0; i<challengeList.length; i++){
@@ -22,27 +22,47 @@ function initializePage() {
     }
   }
 
-  for(var i=0; i<localStorage.length; i++){
-    game = localStorage.key(i);
+  var g = localStorage.getItem("Games");
+  var games2 = JSON.parse(g);
+  if(!g){
+    games2 = [];
+  }
+  for(var i=0; i<games2.length; i++){
+    game = games2[i];
     var challengeList = JSON.parse(localStorage[game]);
     for (var j=0; j<challengeList.length; j++){
       var currData = challengeList[j];
       if (currData["difficulty"] == "easy"){
-        currData["difficulty"] = "text-white bg-success";
-        currData['diffBorder'] = "border-success";
-        currData["diffName"] = 'Easy'
+        currData["difficulty"] = "border-success";
       } else if (currData["difficulty"] == "medium"){
-        currData["difficulty"] = "bg-warning";
-        currData['diffBorder'] = "border-warning";
-        currData["diffName"] = "Medium"
+        currData["difficulty"] = "border-warning";
       } else{
-        currData["difficulty"] = "text-white bg-danger";
-        currData['diffBorder'] = "border-danger";
-        currData["diffName"] = "Hard"
+        currData["difficulty"] = "border-danger";
       }
       currData["GameTitle"] = game;
+      currData["index"] = j;
+      currData["uid"] = game + j;
       var currHtml = template(currData);
       parentDiv.append(currHtml);
     }
   }
+
+}
+
+
+function addToList(title, idx){
+  var myListGames = localStorage.getItem("myListGames");
+  var myListChallenges = localStorage.getItem("myListChallenges");
+  if(myListGames == null){
+    myListGames = [];
+    myListChallenges = [];
+  } else {
+    myListGames = JSON.parse(myListGames);
+    myListChallenges = JSON.parse(myListChallenges);
+  }
+  myListGames.push(title);
+  myListChallenges.push(idx);
+  localStorage.setItem("myListGames", JSON.stringify(myListGames));
+  localStorage.setItem("myListChallenges", JSON.stringify(myListChallenges));
+  $("#" + title+idx).html("Added");
 }
