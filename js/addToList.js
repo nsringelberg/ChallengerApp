7dfc2ac1e6f3daea7da79bc66b0c49488b77f730
Nathan Ringelberg <nsringelberg@gmail.com>
2018-11-08 20:57:45 -0800

    challenges added to My List

---
+++ js/addToList.js
@@ -0,0 +1,11 @@
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+    $("#addLink").click(addToList);
+}
+
+function addToList(){
+  console.log("hi");
+}
